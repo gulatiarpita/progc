d7f37366751cbd7ef198da3f5e74f55a71ac76c3
gulatiarpita6-png <gulatiarpita6@gmail.com>
2025-10-01 10:06:31 +0000

    done

---
+++ q60.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    int pos = 0, neg = 0, ze = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n; i++) {
+        if (arr[i] > 0)
+            pos++;
+        else if (arr[i] < 0)
+            neg++;
+        else
+            ze++;
+    }
+    printf("Positive=%d, Negative=%d, Zero=%d\n", pos, neg, ze);
+    return 0;
+}
